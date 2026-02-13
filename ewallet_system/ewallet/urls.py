87256a39ab3e25a91bec3e0268593f9f1e0e72c0
Sulav-Koirala <koiralasulav0@gmail.com>
2026-02-13 19:00:52 +0545

    added: update user details

--- ewallet_system/ewallet/urls.py
+++ ewallet_system/ewallet/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .api_views import register_user,login_user
+from .api_views import register_user,login_user,update_user
 
 urlpatterns = [
     path("user/register/", register_user, name='register'),
     path("user/login/", login_user, name='login'),
+    path("user/update/", update_user, name='update'),
 ]
